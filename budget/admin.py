d93df994d201423121aba8b19c3c3ea496157019
kflorkowski <k.florkowski98@gmail.com>
2024-11-14 12:37:57 +0100

    admin creation & models registration

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserGoal, Budget, Category, Expense, Income
 
 # Register your models here.
+admin.site.register(UserGoal)
+admin.site.register(Budget)
+admin.site.register(Category)
+admin.site.register(Expense)
+admin.site.register(Income)
