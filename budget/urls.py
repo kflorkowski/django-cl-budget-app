eedf5000e59d21125dc08a247f88777f92433965
kflorkowski <k.florkowski98@gmail.com>
2024-11-14 14:58:00 +0100

    login added

--- budget/urls.py
+++ budget/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
+    path('login', views.login, name='login')
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
