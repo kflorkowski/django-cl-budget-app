0477407fb310136d597df28c373c1175a65618c5
kflorkowski <k.florkowski98@gmail.com>
2024-11-14 15:38:58 +0100

    budget form added

--- budget/forms.py
+++ budget/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Budget
 
 
 class UserRegisterForm(UserCreationForm):
@@ -13,3 +14,13 @@ class UserRegisterForm(UserCreationForm):
 
 class UserLoginForm(AuthenticationForm):
     pass
+
+class BudgetForm(Budget):
+    class Meta:
+        model = Budget
+        fields = ('name', 'date')
+        labels = {
+            'name':'Budget name',
+            'date':'Date',
+        }
+            
