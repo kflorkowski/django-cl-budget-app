8e7048bd875dbff2bf08886e6aa17d4be19af300
kflorkowski <k.florkowski98@gmail.com>
2024-11-14 15:05:39 +0100

    register added

--- budget/views.py
+++ budget/views.py
@@ -7,6 +7,18 @@ from django.contrib.auth import authenticate, login, logout
 def base(request):
     return render(request, 'base.html')
 
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
 def login(request):
     if request.method == "POST":
         form = UserLoginForm(request.POST)
