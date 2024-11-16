cc91e31eef73ad8cd4879d7640227acc07eff173
kflorkowski <k.florkowski98@gmail.com>
2024-11-17 00:21:53 +0100

    budget model deleted & Goal, contribution models added

--- budget/models.py
+++ budget/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserGoal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    goal_name = models.CharField(max_length=64)
-    goal_description = models.CharField(max_length=512)
-    goal_value = models.DecimalField(max_digits=21, decimal_places=2)
+class Goal(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    contributor = models.ManyToManyField(User, through='Contribution')
+    target_amount = models.DecimalField(max_digits=21, decimal_places=2)
+
+
+class Contribution(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=21, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username} - {self.goal_name}'
+        return f"{self.contributor} → {self.goal}: {self.amount}"
 
 
 class Category(models.Model):
@@ -17,23 +25,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
-
-class Budget(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=128)
-    date = models.DateField()
-    category = models.ManyToManyField(Category, related_name='category')
-
-    def __str__(self):
-        return f'{self.name} - {self.date.strftime("%B %Y")}'
 
 
 class Expense(models.Model):
     name = models.CharField(max_length=128)
     amount = models.DecimalField(max_digits=21, decimal_places=2)
     category = models.ForeignKey(Category, related_name='expense', on_delete=models.CASCADE)
-    budget = models.ForeignKey(Budget, related_name='expense', on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.name}: {self.amount}'
@@ -43,7 +41,7 @@ class Income(models.Model):
     name = models.CharField(max_length=128)
     amount = models.DecimalField(max_digits=21, decimal_places=2)
     category = models.ForeignKey(Category, related_name='income', on_delete=models.CASCADE)
-    budget = models.ForeignKey(Budget, related_name='income', on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.name}: {self.amount}'
