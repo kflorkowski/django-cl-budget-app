250156de45c966df17eb542d11c94f7a76b8516f
kflorkowski <k.florkowski98@gmail.com>
2024-11-17 10:52:59 +0100

    income & expense - forms added

--- budget/forms.py
+++ budget/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Income, Expense
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,3 +13,31 @@ class UserRegisterForm(UserCreationForm):
 
 class UserLoginForm(AuthenticationForm):
     pass
+
+class IncomeForm(forms.ModelForm):
+    class Meta:
+        model = Income
+        fields = ('name', 'category', 'amount', 'date')
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
+    
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount <= 0:
+            raise forms.ValidationError("Amount must be a positive number.")
+        return amount
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ('name', 'category', 'amount', 'date')
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount <= 0:
+            raise forms.ValidationError("Amount must be a positive number.")
+        return amount
