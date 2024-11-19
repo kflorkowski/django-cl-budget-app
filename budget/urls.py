3a6259efdea05f51e57c93fba35f4b0e1b59c57a
kflorkowski <k.florkowski98@gmail.com>
2024-11-19 02:54:57 +0100

    budget displeing at dashboard

--- budget/urls.py
+++ budget/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
+from .views import DashboardView
 
 urlpatterns = [
     path('', views.base, name='base'),
     path('login', views.user_login, name='login'),
     path('register', views.user_register, name='register'),
     path('logout', views.user_logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
     path('budgets', views.budgets ,name='budgets'),
-    path('goals', views.goals ,name='goals'),
+    path('goals', views.goals, name='goals'),
     path('goals/add-goal', views.add_goal ,name='add_goal'),
     path('goals/donate/<int:goal_id>', views.donation, name='donation'),
     path('transactions', views.transactions ,name='transactions'),
--- budget/views.py
+++ budget/views.py
@@ -3,9 +3,11 @@ from django.contrib import messages
 from .forms import UserRegisterForm, UserLoginForm, IncomeForm, ExpenseForm, GoalForm, ContributionForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import Income, Expense, Goal, Contribution
+from .models import Income, Expense, Goal, Contribution, Category
 from django.db.models import Sum
 from datetime import datetime
+from django.views.generic import TemplateView
+from django.db import models
 
 # Create your views here.
 def base(request):
@@ -42,31 +44,79 @@ def user_logout(request):
     logout(request)
     return redirect('login')
 
-@login_required
-def dashboard(request):
-    # budgets = Budget.objects.filter(owner=request.user)
-    user_goals = Goal.objects.filter(owner=request.user)
-    user_contribution = Contribution.objects.filter(contributor=request.user)
-    other_contribution = Contribution.objects.exclude(contributor=request.user).filter(goal__in=user_goals)
+class DashboardView(TemplateView):
+    template_name = "dashboard.html"
 
-    context = {
-    #     'budgets': budgets,
-        'user_goals': user_goals,
-        'user_contribution': user_contribution,
-        'other_contribution': other_contribution,
-    }
-    return render(request, 'dashboard.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        user_goals = Goal.objects.filter(owner=self.request.user)
+        goals_with_progress = []
+        for goal in user_goals:
+            total_contributions = Contribution.objects.filter(goal=goal).aggregate(total=Sum('amount'))['total'] or 0
+            progress = (total_contributions / goal.target_amount) * 100 if goal.target_amount > 0 else 0
+            goals_with_progress.append({
+                'goal': goal,
+                'total_contributions': total_contributions,
+                'progress': progress,
+            })
+        
+        user_contribution = Contribution.objects.filter(contributor=self.request.user)
+        other_contribution = Contribution.objects.exclude(contributor=self.request.user).filter(goal__in=user_goals)
+        
+        last_month = datetime.now().month - 1 if datetime.now().month > 1 else 12
+        category_summary = []
+        
+        total_expenses = 0
+        total_incomes = 0
+        
+        for category in Category.objects.all():
+            category_expenses = Expense.objects.filter(category=category, user=self.request.user, date__month=last_month)
+            category_incomes = Income.objects.filter(category=category, user=self.request.user, date__month=last_month)
 
-@login_required
-def budgets(request):
-    return render(request, 'budgets.html')
+            total_expenses_in_category = category_expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+            total_incomes_in_category = category_incomes.aggregate(Sum('amount'))['amount__sum'] or 0
 
+            category_summary.append({
+                'category': category,
+                'total_expenses_in_category': total_expenses_in_category,
+                'total_incomes_in_category': total_incomes_in_category,
+            })
+            
+            total_expenses += total_expenses_in_category
+            total_incomes += total_incomes_in_category
+        
+        total_balance = total_incomes - total_expenses
+        
+        context.update({
+            'user_goals': goals_with_progress,
+            'user_contribution': user_contribution,
+            'other_contribution': other_contribution,
+            'category_summary': category_summary,
+            'total_expenses': total_expenses,
+            'total_incomes': total_incomes,
+            'total_balance': total_balance,
+        })
+        return context
+    
 @login_required
 def goals(request):
     my_goals = Goal.objects.filter(owner=request.user)
     others_goals = Goal.objects.exclude(owner=request.user)
-    return render(request, 'goals.html', {'my_goals':my_goals, 'others_goals':others_goals})
 
+    for goal in my_goals:
+        total_contributions = Contribution.objects.filter(goal=goal).aggregate(total_amount=models.Sum('amount'))['total_amount'] or 0
+        goal.current_amount = total_contributions
+        goal.current_percentage = round((total_contributions / goal.target_amount) * 100, 2) if goal.target_amount > 0 else 0
+
+    for goal in others_goals:
+        total_contributions = Contribution.objects.filter(goal=goal).aggregate(total_amount=models.Sum('amount'))['total_amount'] or 0
+        goal.current_amount = total_contributions
+        goal.current_percentage = round((total_contributions / goal.target_amount) * 100, 2) if goal.target_amount > 0 else 0
+
+    return render(request, 'goals.html', {'my_goals': my_goals, 'others_goals': others_goals})
+
+@login_required
 def add_goal(request):
     if request.method == 'POST':
         form = GoalForm(request.POST)
@@ -79,6 +129,7 @@ def add_goal(request):
         form = GoalForm()
     return render(request, 'add_goal.html', {'form':form})
 
+@login_required
 def donation(request, goal_id):
     goal = get_object_or_404(Goal, id=goal_id)
 
@@ -126,6 +177,7 @@ def add_expense(request):
         form = ExpenseForm()
     return render(request, 'add_expense.html', {'form': form})
 
+@login_required
 def edit_income(request, transaction_id):
     transaction = get_object_or_404(Income, id=transaction_id)
     form = IncomeForm(request.POST or None, instance=transaction)
@@ -138,7 +190,7 @@ def edit_income(request, transaction_id):
             return redirect('transactions')
     return render(request, 'edit_transaction.html', {'form': form, 'type': 'income'})
 
-
+@login_required
 def edit_expense(request, transaction_id):
     transaction = get_object_or_404(Expense, id=transaction_id)
     form = ExpenseForm(request.POST or None, instance=transaction)
@@ -151,6 +203,7 @@ def edit_expense(request, transaction_id):
             return redirect('transactions')
     return render(request, 'edit_transaction.html', {'form': form, 'type': 'expense'})
 
+@login_required
 def budgets(request):
     start_date = request.GET.get('start_date', datetime.today().replace(day=1).strftime('%Y-%m-%d'))
     end_date = request.GET.get('end_date', datetime.today().strftime('%Y-%m-%d'))
